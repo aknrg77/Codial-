bdcb2b291ba33895753aa141405697379cb10687
Anurag <aknrg627@gmail.com>
2019-10-05 03:09:47 +0530

    add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function (req,res){
+
+    res.send('<h1>The express is running up for the codiel');
+}
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const port = 8000;
 
 
+
+// using express router
+app.use('/',require('./routes'))
+
+
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server : ${err}`);
